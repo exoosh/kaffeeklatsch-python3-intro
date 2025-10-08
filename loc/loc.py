ca878989026166a5d5f0ebbd29495952f094afe0
Oliver Schneider <oschneider@exocad.com>
2025-10-08 15:13:17 +0200

    Progress from 2025-10-08

--- loc/loc.py
+++ loc/loc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 from pathlib import Path
+from collections import defaultdict
 
 # import humptydumpty
 # ^--- uncomment to see modules at work, this only works with modern Python 3.x
@@ -14,29 +15,50 @@ from pathlib import Path
 # Experiment: switch out mod for mod2 to import another part of the package
 
 
+def count_non_empty(fileobj):
+    linectr = 0
+    while line := fileobj.readline():  # read line-wise
+        # strip blank space (incl. \n) from both sides of string
+        stripped_line = line.strip()
+        # is the string non-empty?
+        if stripped_line:
+            linectr += 1  # actual counting of non-empty lines here (TODO)
+    return linectr
+
+
 def count(p):
-    extensions = {".cs", ".pas", ".c", ".h"}  # this is a set (type) literal
-    if p.suffix in extensions:  # this tests membership in the set
-        with p.open() as f:  # open with context manager using pathlib.Path.open()
-            while line := f.readline():  # read line-wise
-                # print(f"{line=!r}") # debug output
-                # strip blank space (incl. \n) from both sides of string
-                stripped_line = line.strip()
-                # print(f"{stripped_line=!r}") # debug output
-                # is the string non-empty?
-                if stripped_line:
-                    ...  # actual counting of non-empty lines here (TODO)
+    # NB: we open as binary so files that aren't UTF-8 encoded can be handled
+    with p.open("rb") as f:  # open with context manager using pathlib.Path.open()
+        linectr = count_non_empty(f)
+        return linectr, p.suffix
+    return None, None
 
 
 def main(*args):
+    extensions = {
+        ".foo": "foobar baz",  # doesn't exist as "file type"
+        ".cs": "C#",
+        ".pas": "Pascal/Delphi",
+        ".c": "C source",
+        ".h": "C header",
+    }  # this is a dict (type) literal
+    # defaultdict creates a key upon access, default based on the type
+    count_dict = defaultdict(int)
     for file_or_dir_path in args:  # iterate over command line arguments
         p = Path(file_or_dir_path)  # instantiate pathlib.Path from argument (str)
         # recursively glob through the path (globs are approx. wildcards)
-        for path_item in p.rglob("*"):
-            if path_item.is_file():  # for files we proceed
-                count(path_item)  # ... to call the count() function
-                break  # just here temporarily  break after the first file
-        break  # just here temporarily  to break after the first command line arg
+        for p in p.rglob("*"):
+            # for files with matching extension we proceed
+            if p.is_file() and p.suffix in extensions:
+                lines, suffix = count(p)  # ... to call the count() function
+                if lines and suffix:
+                    count_dict[suffix] += lines
+    # iterating over a dictionary is done using the .items() method
+    for extension, description in extensions.items():
+        # If this is one of the extensions we care about, proceed
+        if extension in count_dict:
+            # output the "language description" and cumulative line count
+            print(f"{description}: {count_dict[extension]}")
 
 
 if __name__ == "__main__":
